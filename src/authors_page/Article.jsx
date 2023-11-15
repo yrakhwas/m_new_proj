2cf45de27ba933bf78f190e79807211e761f1c55
yrakhwas <yrakhwas@gmail.com>
2023-11-15 23:10:39 +0200

    Add some info about author

--- src/authors_page/Article.jsx
+++ src/authors_page/Article.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import AuthorsInfo from './AuthorsInfo';
 import SocialLinks from './SocialLinks';
+import './Article.css';
 
 function Article()
 {
     return(
-        <div>
-            <h1>Header of title</h1>
-            <AuthorsInfo authorName = "Mark Twen"/>
-            <p>Content here.....</p>
+        <div class="container">
+            <h1>Tom Sawyer and Huckleberry Finn</h1>
+            <AuthorsInfo authorName = "Mark Twain"/>
+                <p>Chapter about...</p>
             <SocialLinks/>
         </div>
     );
